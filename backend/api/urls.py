a58d1d4781f38cc02c3ee8c052d0a6238bfdb0e0
NoMoreTrouble <fossaluzza.matteo@gmail.com>
2025-02-23 19:25:58 +0100

    [19 19-setup-django-react-vite-tailwind-flowbite] Realized OutputTest page as CV visualization in order to test new API from data.json in backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ItemListCreate
+from .views import ItemListCreate, get_json_data
 
 urlpatterns = [
     path('items/', ItemListCreate.as_view(), name='item-list-create'),
+    path('data/', get_json_data, name='get_json_data')
+
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,8 @@
+import os
+import json
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.db import models
 from .models import Item
 from .serializers import ItemSerializer
@@ -10,3 +13,13 @@ from rest_framework import generics
 class ItemListCreate(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+def get_json_data(request):
+# Adjust the path to the data.json file inside your api/data folder
+    file_path = os.path.join(os.path.dirname(__file__), "data", "data.json")
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        return JsonResponse(data, safe=False)
+    except FileNotFoundError:
+        return JsonResponse({"error": "File not found"}, status=404)
