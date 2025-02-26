9907bfa99edc962583fc299dbdaaeb419c3de8e6
federico.toldo <federico.toldo@smc.it>
2025-02-26 21:53:10 +0100

    #main merge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'flowbite';
-import './index.css'
-import App from './App.tsx'
->>>>>>> c950a27049e42b0509ec026487062d35f05fbf1b
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
