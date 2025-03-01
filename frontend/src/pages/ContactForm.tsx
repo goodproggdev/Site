32ce1cf2c148293482ad68eedfdda66ef99ca3d6
federico.toldo <federico.toldo@smc.it>
2025-03-01 15:47:53 +0100

    contact

--- frontend/src/pages/ContactForm.tsx
+++ frontend/src/pages/ContactForm.tsx
@@ -32,7 +32,7 @@ const ContactForm = () => {
   };
 
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-gray-900" id="contact">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
           Contact Us
@@ -62,7 +62,7 @@ const ContactForm = () => {
               value={formData.email}
               onChange={handleChange}
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
+              placeholder="example@gmail.com"
               required
               disabled={isSubmitted}
             />
