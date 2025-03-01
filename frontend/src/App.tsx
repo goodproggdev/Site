a1d85c5fe44816ded16eec354bbcd3ccab0b1055
federico.toldo <federico.toldo@smc.it>
2025-03-01 12:46:38 +0100

    WIP:header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import { DarkThemeToggle } from "flowbite-react";
 import { Navbar, Footer } from "./layout";
 import { Welcome, Preview, About, Statistics, Testimonials, ContactForm, Feature } from "./pages";
 
 function App() {
   return (
     <>
-    <Navbar />
+    <Navbar />   
     <Welcome />
     <Preview />
     <About />
@@ -13,10 +12,6 @@ function App() {
     <Statistics />
     <Testimonials />
     <ContactForm />
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-      <h1 className="text-2xl dark:text-white">Flowbite React + Vite</h1>
-      <DarkThemeToggle />      
-    </main>
     <Footer />
     </>
   );
