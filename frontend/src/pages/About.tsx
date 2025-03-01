f8dc5b293021e4298427c93bbf4c64c49c794340
federico.toldo <federico.toldo@smc.it>
2025-03-01 17:31:32 +0100

    links

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,6 +1,6 @@
 const About = () => {
     return(
-        <section className="bg-white dark:bg-gray-900">
+        <section id="about" className="bg-white dark:bg-gray-900">
     <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
