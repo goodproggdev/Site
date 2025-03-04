3c8a22ec87f511f1305a01231145647e54730340
federico.toldo <federico.toldo@smc.it>
2025-03-04 14:41:58 +0100

    button visible

--- frontend/src/pages/ContactForm.tsx
+++ frontend/src/pages/ContactForm.tsx
@@ -166,7 +166,7 @@ const ContactForm = () => {
 
           <button
             type="submit"
-            className={`py-3 px-5 text-sm font-medium text-center text-white rounded-lg transition-colors sm:w-fit focus:ring-4 focus:outline-none ${
+            className={`py-3 px-5 text-sm font-medium text-center dark:text-white rounded-lg transition-colors sm:w-fit focus:ring-4 focus:outline-none ${
               status === "loading"
                 ? "bg-gray-400 cursor-not-allowed"
                 : "bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
